468e788c899717df03d270630913b90be42cc029
Deepak Kumar <deepakkumarnd@gmail.com>
2018-09-12 23:40:47 +0530

    Add device views

--- config/puma.rb
+++ config/puma.rb
@@ -9,12 +9,16 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
-bind 'unix:///home/deploy/quillpad/shared/tmp/sockets/puma.sock'
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+if ENV.fetch("RAILS_ENV") == 'production'
+  bind 'unix:///home/deploy/quillpad/shared/tmp/sockets/puma.sock'
+else
+  port        ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
