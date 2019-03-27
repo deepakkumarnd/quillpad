8ea9c51c22b99c90ac408c552a0bec0a3a8f90c2
deepak.kumar <deepak.kumar@truecaller.com>
2019-03-27 12:03:27 +0530

    add status check end point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def status
+    response = {
+      app: 'ok',
+      db: db_connection_count
+    }
+
+    render status: get_status_code(response), json: response
+  end
+
+  private
+
+  def db_connection_count
+    ActiveRecord::Base.connection.execute("select count(*) from pg_stat_activity where pid <> pg_backend_pid() and usename = current_user;")
+      .first["count"]
+  rescue => e
+    -1
+  end
+
+  def get_status_code(response)
+    return 600 if response[:db] < 0
+    200
+  end
 end
