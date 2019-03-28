5262c107015ef96e5a9f560c353575c2971a0155
deepak.kumar <deepak.kumar@truecaller.com>
2019-03-28 23:51:03 +0530

    set username automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
 
   has_many :posts
+  validates :subdomain, uniqueness: true
+  before_validation :set_random_username
+
+  def set_random_username
+    return if self.subdomain.present?
+
+    10.times do
+      str = SecureRandom.urlsafe_base64(8).downcase
+      yes = User.exists?(subdomain: str)
+
+      if !yes
+        self.subdomain = str
+        break
+      end
+    end
+  end
 end
