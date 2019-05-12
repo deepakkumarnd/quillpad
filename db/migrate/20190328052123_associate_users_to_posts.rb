52691a0e3f4a5033aeb0cc7802776d903c7fbdef
deepak.kumar <deepak.kumar@truecaller.com>
2019-05-12 10:54:20 +0530

    disable migration which are not in use

--- db/migrate/20190328052123_associate_users_to_posts.rb
+++ db/migrate/20190328052123_associate_users_to_posts.rb
@@ -1,6 +1,9 @@
 class AssociateUsersToPosts < ActiveRecord::Migration[5.1]
   def change
-    user_id = User.last.id
-    Post.update_all(user_id: user_id)
+    # TODO: This migration is written to do data changes and is not likely to be used in future.
+    return
+
+    user_id = User.last&.id
+    Post.update_all(user_id: user_id) if user_id.present?
   end
 end
--- db/migrate/20190328052628_add_posts_count_to_counter_cache.rb
+++ db/migrate/20190328052628_add_posts_count_to_counter_cache.rb
@@ -1,6 +1,11 @@
 class AddPostsCountToCounterCache < ActiveRecord::Migration[5.1]
   def change
+    # TODO: This migration is written to do data changes and is not likely to be used in future.
+    return
+
     user  = User.last
-    ActiveRecord::Base.connection.execute("UPDATE users SET posts_count = #{user.posts.count} WHERE id = #{user.id}")
+    if user.present?
+      ActiveRecord::Base.connection.execute("UPDATE users SET posts_count = #{user.posts.count} WHERE id = #{user.id}")
+    end
   end
 end
