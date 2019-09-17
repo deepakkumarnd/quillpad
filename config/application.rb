3b64b89557fbe3d74e81af9f8fc2136052ff1b67
Deepak Kumar <deepakkumar@gmail.com>
2019-09-17 23:22:58 +0530

    Updating rails to 6.0.0

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,7 +12,8 @@ module Quillpad
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,30 +1,33 @@
-daemonize (ENV['RAILS_ENV'] == "production")
-
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+daemonize (ENV['RAILS_ENV'] == "production")
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# Specifies the `pidfile` that Puma will use.
 if ENV.fetch("RAILS_ENV") == 'production'
   bind "unix:///#{ENV['HOME']}/quillpad/shared/tmp/sockets/puma.sock"
   pidfile "#{ENV['HOME']}/quillpad/shared/tmp/pids/puma.pid"
 else
   port        ENV.fetch("PORT") { 3000 }
+  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 end
 
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
+# Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
