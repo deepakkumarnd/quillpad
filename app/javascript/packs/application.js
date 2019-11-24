fbcf79af3ada82e63d4847c046b7f3512f2288c3
Deepak Kumar <deepakkumar@gmail.com>
2019-11-24 10:56:33 +0530

    load rails/ujs via webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,9 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import Rails from  "@rails/ujs";
+
+(function(){
+  Rails.start();
+  console.log('Hello World from Webpacker')
+})();
