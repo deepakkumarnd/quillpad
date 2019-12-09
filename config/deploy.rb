4563075e528c8f984e390a1879cb34d78ae19e44
Deepak Kumar <deepakkumar@gmail.com>
2019-12-09 19:47:27 +0530

    add node_modules to the linked directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/deploy/quillpad"
 append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node_modules"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
