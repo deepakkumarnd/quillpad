f8de291aa949ef7073333b1b4e81f878b250bf66
Deepak <deepakkumarnd@gmail.com>
2020-04-26 00:45:09 +0530

    Update repo URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "quillpad"
-set :repo_url, "git@github.com:kumarn33/quillpad.git"
+set :repo_url, "git@github.com:basicobject/quillpad.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
